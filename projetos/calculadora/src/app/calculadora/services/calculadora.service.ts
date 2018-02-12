91c8897984ec5bb08a794da248ba4162de1d8a31
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2018-02-12 21:16:50 -0200

    Documentando o serviço CalculadoraService

--- projetos/calculadora/src/app/calculadora/services/calculadora.service.ts
+++ projetos/calculadora/src/app/calculadora/services/calculadora.service.ts
@@ -1,8 +1,19 @@
+/**
+ * Serviço responsável por executar as operações da 
+ * calculadora.
+ * 
+ * @author Ricardo A Ruiz<ricardo.almendro.ruiz@gmail.com>
+ * @since 1.0.0
+ */
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class CalculadoraService {
 
+  /*
+    Define as constantantes utilizadas para identificar 
+    as operações de cálculo
+  */
   static readonly SOMA: string = '+';
   static readonly SUBTRACAO: string = '-';
   static readonly DIVISAO: string = '/';
@@ -11,7 +22,11 @@ export class CalculadoraService {
   constructor() { }
 
   /**
-   * Método para realizar o cálculo matemático
+   * Método para realizar o cálculo matemático dado 
+   * dois números.
+   * Suporta as operações soma, subtração, divisão
+   * e multiplicação.
+   * 
    * @param num1 primeiro número informado para o cálculo
    * @param num2 segundo número informado para o cálculo
    * @param operacao operação matemática informada
