f503117abf1539c36389bf789c79422fd1c345cb
Ricardo Ruiz <ralmendro@ciandt.com>
2018-02-14 11:19:30 -0200

    Implementando o componente CalculadoraComponent

--- projetos/calculadora/src/app/calculadora/components/calculadora.component.ts
+++ projetos/calculadora/src/app/calculadora/components/calculadora.component.ts
@@ -24,6 +24,11 @@ import { CalculadoraService } from '../services';
  */
 export class CalculadoraComponent implements OnInit {
 
+  private numero1: string;
+  private numero2: string;
+  private resultado: number;
+  private operacao: string;
+
   /** 
    * Construtor do componente.
    * Aqui está definida a injeção do serviço CalculadoraService
@@ -36,6 +41,107 @@ export class CalculadoraComponent implements OnInit {
   * Local mais indicado para inicializar todos os recursos do componente.
   */
   ngOnInit() {
+    this.limpar();
+  }
+
+  /**
+   * Inicializa todos os operadores para os valores padrão.
+   * 
+   * @return void
+   */
+  limpar(): void {
+    this.numero1 = '0';
+    this.numero2 = '0';
+    this.resultado = null;
+    this.operacao = null;
+  }
+
+  adicionarNumero(numero: string): void {
+    if (this.operacao === null) {
+      this.numero1 = this.concatenarNumero(this.numero1, numero);
+    } else {
+      this.numero2 = this.concatenarNumero(this.numero2, numero);
+    }
+  }
+
+  /**
+   * Retorna o valor concatenado. Trata o separador decimal.
+   * 
+   * @param numAtual 
+   * @param numConcat 
+   */
+  concatenarNumero(numAtual: string, numConcat: string): string {
+    if (numAtual === '0' || numAtual === null) {
+      numAtual = '';
+    }
+
+    if (numConcat === '.' && numAtual === '') {
+      return '0.';
+    }
+
+    if (numConcat === '.' && numAtual.indexOf('.') > -1) {
+      return numAtual;
+    }
+
+    return numAtual + numConcat;
+  }
+
+  /**
+   * Caso já possua uma operação selecionada, executa a
+   * operação anterior e define a nova operação.
+   * @param operacao 
+   */
+  definirOperacao(operacao: string): void {
+    //apenas define a operação caso não exista
+    if (this.operacao === null) {
+      this.operacao = operacao;
+      return;
+    }
+
+    /* caso a operação definida e numero 2 selecionado,
+       efetua o cálculo da operação.*/
+    if (this.numero2 !== null) {
+      this.resultado = this.calculadoraService.calcular(
+        parseFloat(this.numero1),
+        parseFloat(this.numero2),
+        this.operacao);
+
+      this.operacao = operacao;
+      this.numero1 = this.resultado.toString();
+      this.numero2 = null;
+      this.resultado = null;
+    }
+  }
+
+  /**
+   * Efetua o cálculo de uma operação.
+   * 
+   * @return void
+   */
+  calcular(): void {
+    if (this.numero2 === null) {
+      return;
+    }
+
+    this.resultado = this.calculadoraService.calcular(
+      parseFloat(this.numero1),
+      parseFloat(this.numero2),
+      this.operacao);
+  }
+
+  /**
+   * Retorna o vaor a ser exubudo na tela da calculadora.
+   * 
+   * @return string
+   */
+  get display(): string {
+    if (this.resultado !== null) {
+      return this.resultado.toString();
+    }
+    if (this.numero2 !== null) {
+      return this.numero2;
+    }
+    return this.numero1;
   }
 
 }
