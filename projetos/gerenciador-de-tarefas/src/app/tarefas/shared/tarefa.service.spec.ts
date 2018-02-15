12234c23b03b76ee6f44715b629acfd8d1182871
Ricardo Ruiz <ralmendro@ciandt.com>
2018-02-15 11:03:39 -0200

    Criando o serviço de gerenciamento de tarefas - parte 2/2

--- projetos/gerenciador-de-tarefas/src/app/tarefas/shared/tarefa.service.spec.ts
+++ projetos/gerenciador-de-tarefas/src/app/tarefas/shared/tarefa.service.spec.ts
@@ -1,9 +1,11 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { TarefaService } from './tarefa.service';
+import { Tarefa } from './tarefa.model';
 
 describe('TarefaService', () => {
   beforeEach(() => {
+    localStorage.clear();
     TestBed.configureTestingModule({
       providers: [TarefaService]
     });
@@ -12,4 +14,77 @@ describe('TarefaService', () => {
   it('should be created', inject([TarefaService], (service: TarefaService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('deve cadastrar uma nova tarefa', 
+    inject([TarefaService], (service: TarefaService) => {
+      let tarefa = new Tarefa(null,'Tarefa 01',false);
+      let idGerado = service.cadastrar(tarefa);
+      let tarefaCadastrada = service.buscarPorId(idGerado);
+      expect(tarefa.nome).toEqual(tarefaCadastrada.nome);
+    })
+  );
+
+  it('deve alterar uma tarefa existente',
+    inject([TarefaService], (service: TarefaService) => {
+
+      let tarefa = new Tarefa(null,'Tarefa 01',false);
+      let idGerado = service.cadastrar(tarefa);
+
+      let tarefaCadastrada = service.buscarPorId(idGerado);
+      expect(tarefaCadastrada.nome).toEqual('Tarefa 01');
+
+      tarefa.nome = 'Tarefa alterada';
+      service.atualizar(tarefa);
+
+      tarefaCadastrada = service.buscarPorId(idGerado);
+      expect(tarefaCadastrada.nome).toEqual('Tarefa alterada');
+
+    })
+  );
+
+  it('deve alterar o status tarefa existente',
+    inject([TarefaService], (service: TarefaService) => {
+
+      let tarefa = new Tarefa(null,'Tarefa 01',false);
+      let idGerado = service.cadastrar(tarefa);
+
+      let tarefaCadastrada = service.buscarPorId(idGerado);
+      expect(tarefaCadastrada.concluida).toEqual(false);
+
+      service.alterarStatus(idGerado);
+
+      tarefaCadastrada = service.buscarPorId(idGerado);
+      expect(tarefaCadastrada.concluida).toEqual(true);
+
+    })
+  );
+
+  it('deve remover uma tarefa cadastrada', 
+    inject([TarefaService], (service: TarefaService) => {
+      let tarefa = new Tarefa(null,'Tarefa 01',false);
+      let idGerado = service.cadastrar(tarefa);
+
+      let tarefaCadastrada = service.buscarPorId(idGerado);
+      expect(tarefaCadastrada).not.toBeNull;
+
+      service.remover(idGerado);
+      tarefaCadastrada = service.buscarPorId(idGerado);
+      expect(tarefaCadastrada).toBeNull;
+
+    })
+  );
+
+  it('deve listar todas as tarefas cadastradas', 
+    inject([TarefaService], (service: TarefaService) => {
+      service.cadastrar(new Tarefa(null,'Tarefa 01'));
+      service.cadastrar(new Tarefa(null,'Tarefa 02'));
+      service.cadastrar(new Tarefa(null,'Tarefa 03'));
+      
+      let tarefas: Tarefa[] = service.listarTodos();
+      expect(tarefas).not.toBeNull;
+      expect(tarefas).length === 3;
+      
+    })
+  );
+
 });
--- projetos/gerenciador-de-tarefas/src/app/tarefas/shared/tarefa.service.ts
+++ projetos/gerenciador-de-tarefas/src/app/tarefas/shared/tarefa.service.ts
@@ -22,10 +22,72 @@ export class TarefaService {
    * 
    * @param tarefa 
    */
-  cadastrar(tarefa: Tarefa): void {
+  cadastrar(tarefa: Tarefa): number {
     const tarefas: Tarefa[] = this.listarTodos();
     tarefa.id = new Date().getTime();
     tarefas.push(tarefa);
+    this.persisteListaTarefas(tarefas);
+    return tarefa.id;
+  }
+
+  /**
+   * Busca uma tarefa do localStorage pelo seu id.
+   * 
+   * @param id 
+   */
+  buscarPorId(id: number): Tarefa {
+    const tarefas: Tarefa[] = this.listarTodos();
+    return tarefas.find(tarefa => tarefa.id === id);
+  }
+
+  /**
+   * Atualiza uma tarefa no localStorage.
+   * 
+   * @param tarefa 
+   */
+  atualizar(tarefa: Tarefa): void {
+    const tarefas: Tarefa[] = this.listarTodos();
+    tarefas.forEach((obj, index, objs) => {
+      if (tarefa.id === obj.id) {
+        objs[index] = tarefa;
+      }
+    });
+    this.persisteListaTarefas(tarefas);
+  }
+
+  /**
+   * Remove uma tarefa do localStorage.
+   * 
+   * @param id 
+   */
+  remover(id: number): void {
+    let tarefas: Tarefa[] = this.listarTodos();
+    tarefas = tarefas.filter(tarefa => tarefa.id !== id);
+    this.persisteListaTarefas(tarefas);
+  }
+
+  /**
+   * Altera o status da tarefa no localStorage.
+   * 
+   * @param id 
+   */
+  alterarStatus(id: number): void {
+    const tarefas: Tarefa[] = this.listarTodos();
+    tarefas.forEach((objTarefa, index, objTarefas) => {
+      if (objTarefa.id === id) {
+        objTarefas[index].concluida = !objTarefa.concluida;
+      }
+    });
+    this.persisteListaTarefas(tarefas);
+  }
+
+  /**
+   * Atualiza o chave tarefas no localStorage com uma nova
+   * lista de tarefas informada.
+   * 
+   * @param tarefas lista de tarefas a ser atualizada.
+   */
+  persisteListaTarefas(tarefas: Tarefa[]): void {
     localStorage['tarefas'] = JSON.stringify(tarefas);
   }
 
