1d4a626f14287e99458e8448d3befa6b2acc32ad
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2018-02-18 13:27:07 -0300

    Implementando a diretiva de tarefa concluida

--- projetos/gerenciador-de-tarefas/src/app/tarefas/shared/tarefa-concluida.directive.ts
+++ projetos/gerenciador-de-tarefas/src/app/tarefas/shared/tarefa-concluida.directive.ts
@@ -1,10 +1,33 @@
-import { Directive } from '@angular/core';
+import { 
+  Directive,
+  ElementRef,
+  Input,
+  OnInit
+} from '@angular/core';
 
 @Directive({
-  selector: '[appTarefaConcluida]'
+  selector: '[tarefaConcluida]'
 })
-export class TarefaConcluidaDirective {
+export class TarefaConcluidaDirective implements OnInit {
 
-  constructor() { }
+  /* recebe o valor se a tarefa está concluída do objeto que usa
+     a diretiva.*/
+  @Input() tarefaConcluida: boolean;
+
+  /**
+   * ElementRef fornece através da injeção de dependências do angular
+   * uma referência do elemento que está usando a diretiva.
+   * @param el 
+   */
+  constructor(private el: ElementRef) { }
+
+  /** 
+   * Método da interface OnInit
+  */
+  ngOnInit() {
+    if (this.tarefaConcluida) {
+      this.el.nativeElement.style.textDecoration = "line-through";
+    }
+  }
 
 }
