6fd0120cbd650c404b8af0837939850efe7330cf
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2018-02-18 11:41:59 -0300

    Alterando o status de uma tarefa

--- projetos/gerenciador-de-tarefas/src/app/tarefas/listar/listar-tarefa.component.ts
+++ projetos/gerenciador-de-tarefas/src/app/tarefas/listar/listar-tarefa.component.ts
@@ -31,4 +31,11 @@ export class ListarTarefaComponent implements OnInit {
     }
   }
 
+  alterarStatus(tarefa: Tarefa): void {
+    if (confirm('Deseja alterar o status da tarefa ' + tarefa.nome + '"?"')) {
+      this.tarefaService.alterarStatus(tarefa.id);
+      this.tarefas = this.listarTodos();
+    }
+  }
+
 }
