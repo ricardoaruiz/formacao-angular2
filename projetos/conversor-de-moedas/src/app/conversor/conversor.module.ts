1bd99f1846267291ceece56835e42a2b243a3b14
Ricardo Ruiz <ralmendro@ciandt.com>
2018-02-20 14:03:47 -0300

    Criando o componente do Conversor de moedas

--- projetos/conversor-de-moedas/src/app/conversor/conversor.module.ts
+++ projetos/conversor-de-moedas/src/app/conversor/conversor.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ConversorComponent } from './components';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [
+    ConversorComponent
+  ],
+  exports: [
+    ConversorComponent
+  ]
 })
 export class ConversorModule { }
