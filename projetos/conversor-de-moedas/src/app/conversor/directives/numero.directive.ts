eaca6fef03a12c36cae5e56182e187d8045115b2
Ricardo Ruiz <ralmendro@ciandt.com>
2018-02-20 16:26:04 -0300

    Implementando a diretiva de validação de numero - parte 2/2

--- projetos/conversor-de-moedas/src/app/conversor/directives/numero.directive.ts
+++ projetos/conversor-de-moedas/src/app/conversor/directives/numero.directive.ts
@@ -1,14 +1,32 @@
+//HostListener - anotação que fica escutando os eventos
+//ElemtRef - utilizado para ter acesso ao objeto da tela
 import { 
   Directive,
-  HostListener
+  HostListener,
+  ElementRef
 } from '@angular/core';
 
-@Directive({
-  selector: '[numero]'
-})
-export class NumeroDirective {
+/*Esses imports são necessários para permitir a atualização do 
+  ngModel quando o valor for alterado pela diretiva*/
+import {
+  NG_VALUE_ACCESSOR,
+  ControlValueAccessor
+} from '@angular/forms'
 
-  constructor() { }
+@Directive({
+  selector: '[numero]',
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: NumeroDirective,
+    multi: true
+  }]
+})
+export class NumeroDirective implements ControlValueAccessor {
+
+  onChange: any;
+  onTouched: any;
+
+  constructor(private el: ElementRef) { }
 
   @HostListener('keyup', ['$event'])
   onKeyUp($event: any) {
@@ -23,6 +41,38 @@ export class NumeroDirective {
     }
 
     $event.target.value = valor;
+    this.onChange(valor);
+  }
+
+  /**
+   * Registra a função a ser chamada para atualizar
+   * valor na model para evento change
+   * 
+   * @param fn 
+   */
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  /**
+   * Registra a função a ser chamada para atualizar
+   * valor na model para evento touched
+   * 
+   * @param fn 
+   */
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  /**
+   * Obtém o valor contido na model.
+   * Aqui o valor do elemento é alterado, e por sua vez o valor do model
+   * também, pois estão lincados.
+   * 
+   * @param value 
+   */
+  writeValue(value: any): void {
+    this.el.nativeElement.value = value;
   }
 
 }
