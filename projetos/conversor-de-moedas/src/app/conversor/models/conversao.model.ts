8eb6780278f3045d699867aad920e80490b9f216
Ricardo Ruiz <ralmendro@ciandt.com>
2018-02-20 13:47:08 -0300

    Criando o model conversão

---
+++ projetos/conversor-de-moedas/src/app/conversor/models/conversao.model.ts
@@ -0,0 +1,12 @@
+/** 
+ * Representação de uma conversão.
+*/
+export class Conversao {
+
+    constructor(
+        private moedaDe?: string,
+        private moedaPara?: string,
+        private valor?: number
+    ) {}
+
+}
