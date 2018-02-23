1c1f5b095a9004db8b581f7879bfcaa7fa5df7d2
Ricardo Ruiz <ralmendro@ciandt.com>
2018-02-23 13:51:29 -0300

    Implementando o serviço de dados do Dashboard

--- projetos/dashboard/src/app/dashboard/dados.service.ts
+++ projetos/dashboard/src/app/dashboard/dados.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class DadosService {
 
+  // Array bi-dimensional com dados para exibição no gráfico
+  readonly dados = [
+    ['Janeiro', 33],
+    ['Fevereiro',68],
+    ['Março',49],
+    ['Abril',15],
+    ['Maio',80],
+    ['Junho',27]
+  ];
+
   constructor() { }
+
+  /** 
+   * Retorna um observable com os dados a serem utilizados
+   * para a montagem dos gráficos.
+  */
+  obterDados(): Observable<any> {
+    return new Observable( (observable) => {
+      observable.next(this.dados);
+      observable.complete();
+    });
+  }
 
 }
