1fbad298938df3e28b7785e2fd5640e4b2fc3090
Ricardo Ruiz <ralmendro@ciandt.com>
2018-02-23 09:44:35 -0300

    Implementando o filtro (pipe) de formação de data

--- projetos/conversor-de-moedas/src/app/conversor/pipes/data-br.pipe.ts
+++ projetos/conversor-de-moedas/src/app/conversor/pipes/data-br.pipe.ts
@@ -10,8 +10,25 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DataBrPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  /**
+   * Aplica formatação para a data
+   * 
+   * @param dataEn string dataEn
+   * @returns string data no formato dd/mm/yyyy
+   */
+  transform(dataEn: string,): string {
+
+    if (!dataEn) {
+      return '';
+    }
+
+    const dataArr = dataEn.split('-');
+
+    if (dataArr.length !== 3) {
+      return dataEn;
+    }
+
+    return dataArr[2] + '/' + dataArr[1] + '/' + dataArr[0];
   }
 
 }
